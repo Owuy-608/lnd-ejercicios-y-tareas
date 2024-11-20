227b9860928ca92b48ac8799552ad16fe582e3df
owuy <alejandrobejaranogutierrez@gmail.com>
2024-11-20 11:42:52 +0000

    Tema 2 de lnd ejercicios

---
+++ jonay2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Crear estructura de directorios
+mkdir -p ut02/01-introduccion-a-xml/ejercicio01
+mkdir -p ut02/01-introduccion-a-xml/ejercicio02
+mkdir -p ut02/01-introduccion-a-xml/ejercicio03
+mkdir -p ut02/01-introduccion-a-xml/ejercicio04
+mkdir -p ut02/01-introduccion-a-xml/ejercicio05
+mkdir -p ut02/02-validacion-con-dtd/ejercicio01
+mkdir -p ut02/02-validacion-con-dtd/ejercicio02
+mkdir -p ut02/02-validacion-con-dtd/ejercicio03
+mkdir -p ut02/02-validacion-con-dtd/ejercicio04
+mkdir -p ut02/02-validacion-con-dtd/ejercicio05
+mkdir -p ut02/03-validacion-con-xsd/ejercicio01
+mkdir -p ut02/03-validacion-con-xsd/ejercicio02
+
+echo "Estructura de directorios creada exitosamente."
+
